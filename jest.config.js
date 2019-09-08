3482a433112fce66445754ed8d6af364eda619e3
Karthik Balaji <karthik@auzmor.com>
2019-09-09 04:08:29 +0530

    chore: add lint config & TS setup

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-  cacheDirectory: ".jest-cache",
-  coverageDirectory: ".jest-coverage",
-  coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
-  coverageReporters: ["html", "text"],
+  cacheDirectory: '.jest-cache',
+  coverageDirectory: '.jest-coverage',
+  coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
+  coverageReporters: ['html', 'text'],
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
-  testPathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"]
+  testPathIgnorePatterns: [
+    '<rootDir>/packages/(?:.+?)/lib/',
+    '/node_modules/',
+    '/lib/',
+  ],
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
 };
